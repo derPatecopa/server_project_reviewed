d12b51367897928e796e35e11c5b32051e76e93f
derPatecopa <mb89@me.com>
2023-01-31 15:59:49 +0100

    mainroutes endpoint test added but not working

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -1,5 +1,5 @@
 import request from "supertest";
-import app from "../index";
+import app from "../index"
 
 describe("API endpoint testing", () => {
   it("Should return 200 and correct response for endpoint /api", async () => {
