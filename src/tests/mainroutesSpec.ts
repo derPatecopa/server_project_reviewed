6087ca517a0c455f9c0e1462318ae9629fcb17d4
derPatecopa <mb89@me.com>
2023-01-31 16:55:38 +0100

    added documentation details

--- src/tests/mainroutesSpec.ts
+++
@@ -1,9 +0,0 @@
-// import request from "supertest";
-// import app from "../index"
-
-// describe("API endpoint testing", () => {
-//   it("Should return 200 and correct response for endpoint /api", async () => {
-//     const res = await request(app).get("/api/images");
-//     expect(res.statusCode).toBe(200);
-//   });
-// });
