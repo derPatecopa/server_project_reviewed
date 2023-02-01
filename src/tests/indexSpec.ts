8ba71fc68d2fccc7d0701af20ddeab07910cc6db
derPatecopa <mb89@me.com>
2023-02-01 11:12:56 +0100

    added prettier and lint scripts to work correctly

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -1,9 +1,10 @@
 import request from "supertest";
-import app from "../index"
+import app from "../index";
 
 describe("API endpoint testing", () => {
   it("Should return 200 and correct response for endpoint /api", async () => {
     const res = await request(app).get("/api");
+
     expect(res.statusCode).toBe(200);
   });
 });
